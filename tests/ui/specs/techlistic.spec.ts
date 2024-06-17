d7fecf70e7e248e49a8f0c141f83dc4483d00bbf
LAPTOP-RFDL1QJT\ASUS <idinmaster.1004.2015@gmail.com>
2024-06-17 17:14:08 +0700

    Fix layout to be more simple

--- tests/ui/specs/techlistic.spec.ts
+++ tests/ui/specs/techlistic.spec.ts
@@ -31,32 +31,3 @@ records2.forEach(record => {
         await techlistic.submit();
     });
 });
-
-// test('test', async ({ page }) => {
-//     await page.goto('https://www.techlistic.com/p/selenium-practice-form.html');
-//     await page.locator('input[name="firstname"]').click();
-//     await page.locator('input[name="firstname"]').fill('Good');
-//     await page.locator('input[name="lastname"]').click();
-//     await page.locator('input[name="lastname"]').fill('Morning');
-//     await page.locator('#sex-0').check();
-//     await page.locator('#sex-1').check();
-//     await page.locator('#exp-0').check();
-//     await page.locator('#exp-6').check();
-//     await page.getByText('Years of Experience 1 2 3 4 5 6').click();
-//     await page.locator('#datepicker').click();
-//     await page.locator('#datepicker').fill('12/01/2548');
-//     await page.locator('#profession-0').check();
-//     await page.locator('#profession-1').check();
-//     await page.locator('#tool-0').check();
-//     await page.locator('#tool-1').check();
-//     await page.locator('#tool-2').check();
-//     await page.locator('#continents').selectOption('North America');
-//     await page.locator('#continents').selectOption('Asia');
-//     await page.locator('#selenium_commands').selectOption('WebElement Commands');
-//     await page.locator('#selenium_commands').selectOption('Browser Commands');
-//     await page.locator('#photo').click();
-//     await page.locator('#photo').setInputFiles('RhythianLogo.ico');
-//     await page.getByRole('button', { name: 'Button' }).click();
-//     await page.getByRole('link', { name: 'Click here to Download File' }).click();
-//     await page.frameLocator('[id="google_ads_iframe_\\/1254144\\,22662382379\\/techlistic_com-pixel1_0"]').frameLocator('iframe[name="ad_iframe"]').getByLabel('Close ad').click();
-//   });
